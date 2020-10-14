75c336d1d431c87b3f7377040d4e54edb458e16c
Sundaram Subramanian <learnwithsundar@gmail.com>
2020-10-14 11:17:55 +0530

    Inital Commit
    Initial Commit

---
+++ Foreign exchange rates/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Foreign_exchange_rates.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
